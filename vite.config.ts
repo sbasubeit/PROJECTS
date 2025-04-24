9142141d465cab2802224c03a07d675ee40cbcf0
naifhomood <naifhomood@gmail.com>
2025-04-24 09:49:59 +0300

    نم

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/signed-projects-visual-98/", // Add base path for GitHub Pages
   server: {
     host: "::",
     port: 3001,
